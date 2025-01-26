65c3dda116c295d5a2207c45dd1968efce104140
aavaSapkota <sapkota.aava04@gmail.com>
2025-01-26 10:35:49 -0500

    eslint configs

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 'use client';
 
 import { Paper, Typography, Box, ButtonBase } from '@mui/material';
--- src/context/PatientContext.tsx
+++ src/context/PatientContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 import { createContext, useContext, ReactNode, useState } from 'react';
 import { PatientInfo, PhaseStatus } from '@/types';
